1231828a9b8e352b396c57b6adb76775a2d7a4b2
Ceeg288 <140344319+Ceeg288@users.noreply.github.com>
2024-02-25 17:08:52 +0900

    Fresh Ubuntu Install
    Install packages for coding in an Ubuntu environment (I'm learning Rust so I opted to install that)

---
+++ fresh_ubuntu_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt_installs=("wget" "docker" "zsh" "gpg" "curl" "vim" "code" "tmux" "gcc" "git")
+snap_installs=("postman" "sublime-text" "nvim")
+
+######## GPG keys and other install steps for various programs ########
+
+## Sublime Text ##
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+
+## VS Code ##
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+## Rust ##
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## Update ##
+sudo apt update
+
+######## Installs ########
+
+for package in "${apt_installs[@]}"
+do
+	sudo apt install $package
+done
+
+for snap_package in "${snap_installs[@]}"
+do
+	sudo snap install $snap_package
+done
+
+## Oh my Zsh ##
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
